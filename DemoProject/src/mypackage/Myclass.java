d534004fb9f9928cc52b1fd81538a8a64f4448b4
MullaFa12 <fatimamulla17@gmail.com>
2023-01-31 11:57:04 +0530

    Basic Task

---
+++ DemoProject/src/mypackage/Myclass.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("TestBase");
+		System.out.println("pom.xml");
+		System.out.println("Utilities");
+		System.out.println("Resources");
+		System.out.println("Suit");
+	}
+
+}
